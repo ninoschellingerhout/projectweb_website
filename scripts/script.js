2cb0eb16f77ba3d8fbf378eaba06ffb65ba9fcec
Nino <nino-112@hotmail.com>
2020-10-20 21:20:29 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+// Navigatie menu
+
+var menuButton = document.querySelector("nav button");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
