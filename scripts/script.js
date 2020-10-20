a1474dab66b67aaac323eff4bd64f8e0edc661e7
Nino <nino-112@hotmail.com>
2020-10-20 21:55:47 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,24 @@ function toggleMenu(event) {
   deNav = event.target.parentNode;
   deNav.classList.toggle("toonMenu");
 }
+
+// Smooth scroll on page
+
+const links = document.querySelectorAll("header ul.on-page-navigatie a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
+// source: https://webdesign.tutsplus.com/tutorials/smooth-scrolling-vanilla-javascript--cms-35165
